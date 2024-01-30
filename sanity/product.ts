f35faac34c929b9691289f5fece97be793432bc9
Muhammad Nouman Attique <108257688+kingpin147@users.noreply.github.com>
2024-01-30 14:24:15 +0500

    sanity added

---
+++ sanity/product.ts
@@ -0,0 +1,12 @@
+export const product = {
+    name: 'product',
+    type: 'document',
+    title: 'Product',
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name'
+        }
+    ]
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+     <h1 className="flex min-h-screen flex-col items-center justify-between p-24">hello</h1>
     </main>
   );
 }
